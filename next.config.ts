d8dfc8235f271e2970d8e30d206f183a2bdcd8b7
Amit kumar <amitk.kumar414@gmail.com>
2026-06-05 20:17:10 +0530

    minor change

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  basePath: '/finance-calculator-hub',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/finance-calculator-hub',
-        basePath: false,
-        permanent: false,
-      },
-    ]
-  },
-  /* config options here */
-};
   async redirects() {
     return [
       {
